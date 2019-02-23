75d6726f0651a4de9f28255d1b044b554574eca6
xiplus <huangxuanyuxiplus@gmail.com>
2019-02-23 13:13:37 +0800

    Add webhook url to config

--- set_webhook.py
+++ set_webhook.py
@@ -6,17 +6,18 @@ import os
 import requests
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('action', default='set')
-parser.add_argument('--url')
-parser.add_argument('--max_connections', default=40)
-args = parser.parse_args()
-print(args)
-
 config = configparser.ConfigParser()
 configpath = os.path.dirname(os.path.realpath(__file__)) + '/config.ini'
 config.read(configpath)
+
 token = config.get('telegram', 'token')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('action', default='set')
+parser.add_argument('--url', default=config.get('global', 'url') + 'telegram')
+parser.add_argument('--max_connections', default=config.get('telegram', 'max_connections'))
+args = parser.parse_args()
+print(args)
 
 if args.action == 'set':
     url = "https://api.telegram.org/bot{0}/setWebhook?url={1}&max_connections={2}".format(
